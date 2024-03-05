ed16610ec4f27932baa89914a68ffd3228f1e98f
GuilhermeKochepkiLiveSEO <guilherme.kochepki@liveseo.com.br>
2024-03-05 11:23:24 -0300

    fix: adjusting overflows

--- components/header/Drawers.tsx
+++ components/header/Drawers.tsx
@@ -8,6 +8,7 @@ import { useUI } from "$store/sdk/useUI.ts";
 import { usePlatform } from "$store/sdk/usePlatform.tsx";
 import type { ComponentChildren } from "preact";
 import { lazy, Suspense } from "preact/compat";
+import { headerHeight } from "./constants.ts";
 
 const Menu = lazy(() => import("$store/components/header/Menu.tsx"));
 const Searchbar = lazy(() => import("$store/components/search/Searchbar.tsx"));
@@ -29,17 +30,10 @@ const Aside = (
     children: ComponentChildren;
   },
 ) => (
-  <div class="bg-base-100 grid grid-rows-[auto_1fr] h-full divide-y max-w-[100vw]">
-    <div class="flex justify-between items-center">
-      <strong class="px-4 py-3">
-        <span class="font-medium text-2xl">{title}</span>
-      </strong>
-      {onClose && (
-        <Button class="btn btn-ghost" onClick={onClose}>
-          <Icon id="XMark" size={24} strokeWidth={2} />
-        </Button>
-      )}
-    </div>
+  <div
+    class="bg-base-100 grid grid-rows-[auto_1fr] h-full divide-y max-w-[100vw]"
+    style={{ marginTop: headerHeight }}
+  >
     <Suspense
       fallback={
         <div class="w-screen flex items-center justify-center">
